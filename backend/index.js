9b9e82cd4fa86a80a014bc6f11314e548fd07db6
Justinas <iamjuststan@hotmail.com>
2025-08-07 19:31:04 +0300

    a

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,45 @@
 const http = require("http");
 
+let backendUrl = "/backend"
+const Books = ["nemunai-teka-i-drakono-kalnus"]; // get a list form google drive. for now this will work
+
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Backend is running via CommonJS!\n");
-  } else if (req.url === "/backend") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Backend is running via CommonJS2!\n");
+  if (req.url === backendUrl) {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    console.log("req rawHeaders:", req.rawHeaders);
+    res.end("Backend is running!\n");
+  
+  } else if (req.url === backendUrl + "/books") {
+    
+    try {
+      const json = JSON.stringify(Books);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(json);
+
+    } catch (error) {
+      console.error("Error fetching names:", error);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Server error");
+
+    }
+
+  } 
+  
+  else if (req.url === backendUrl + "/books/:book") {
+
+    const bookName = req.params.book;
+    console.log("Book slug:", bookName);
+    res.send(`You requested the book: ${bookName}`);
+
+
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Not found");
+  
   }
 });
 
-server.listen(console.log("Server listening"));
+// server.listen(console.log("Server listening: " + Date.now()));
+server.listen(3000, () => {
+  console.log("Server listening: " + Date.now());
+});
