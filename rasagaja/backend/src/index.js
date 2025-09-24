2b3e30bd6a0cf9e73f0aeb292bbebe90c74722a7
Justinas <iamjuststan@hotmail.com>
2025-09-24 14:27:59 +0300

    back end and smoe stuff

--- rasagaja/backend/src/index.js
+++ rasagaja/backend/src/index.js
@@ -1,71 +1,73 @@
-const { findIdByName, getBooks, streamFile } = require("./googleDrive.js");
+// const { findIdByName, getBooks, streamFile } = require("./googleDrive.js");
 const express = require("express");
 const app = express();
 const prefix = "/backend";
 
-let Books = ["nemunai-teka-i-drakono-kalnus"];
+let knygos = ["nemunai-teka-i-drakono-kalnus"];
 
-const server = app.listen(0, () => {
-  console.log(`Server listening on ${server.address().port}`);
+app.listen(() => {
+  console.log(`Server listening on`);
 });
 
 app.get(prefix + "/", async (req, res) => {
-  res.send(req.ip);
+  res.send(
+    "Hello (or 'laba'). I see you've stumbled into my back end - that's okay. You can always go back unless you want to look for some data in here, which is possible. You probably won't find anything very interesting... unless you like audiobooks, in which case, sure.",
+  );
   console.log("init get:", req.ip);
 });
 
-app.get(prefix + "/books", async (req, res) => {
-  try {
-    res.json(Books);
-  } catch (error) {
-    console.error("Error fetching names:", error);
-    res.status(500).send("Server error");
-  }
-});
+// app.get(prefix + "/knygos", async (req, res) => {
+//   try {
+//     res.json(knygos);
+//   } catch (error) {
+//     console.error("Error fetching names:", error);
+//     res.status(500).send("Server error");
+//   }
+// });
 
-app.get(prefix + "/books/:books", async (req, res) => {
-  try {
-    const bookName = req.params.books;
+// app.get(prefix + "/:books", async (req, res) => {
+//   try {
+//     const bookName = req.params.books;
 
-    if (Books.includes(bookName)) {
-      let Chapetes = await getBooks();
-      res.json(Chapetes);
-    } else {
-      console.error("Error fetching book:", bookName);
-      res.status(404).send("No book of that name found");
-    }
-  } catch (error) {
-    console.error("Error fetching book:", error);
-    res.status(500).send("Server error");
-  }
-});
+//     if (Books.includes(bookName)) {
+//       let Chapetes = await getBooks();
+//       res.json(Chapetes);
+//     } else {
+//       console.error("Error fetching book:", bookName);
+//       res.status(404).send("No book of that name found");
+//     }
+//   } catch (error) {
+//     console.error("Error fetching book:", error);
+//     res.status(500).send("Server error");
+//   }
+// });
 
-app.get(prefix + "/books/:book/:chapeter", async (req, res) => {
-  try {
-    const WhatWasSelected = req.params.chapeter;
+// app.get(prefix + "/books/:book/:chapeter", async (req, res) => {
+//   try {
+//     const WhatWasSelected = req.params.chapeter;
 
-    const ListOfChapetes = await getBooks();
+//     const ListOfChapetes = await getBooks();
 
-    if (ListOfChapetes.includes(WhatWasSelected)) {
-      const id = await findIdByName(WhatWasSelected);
-      const stream = await streamFile(id);
+//     if (ListOfChapetes.includes(WhatWasSelected)) {
+//       const id = await findIdByName(WhatWasSelected);
+//       const stream = await streamFile(id);
 
-      res.setHeader("Content-Type", "audio/wav");
-      res.setHeader("Transfer-Encoding", "chunked");
-      res.setHeader("Cache-Control", "no-cache");
+//       res.setHeader("Content-Type", "audio/wav");
+//       res.setHeader("Transfer-Encoding", "chunked");
+//       res.setHeader("Cache-Control", "no-cache");
 
-      stream.on("error", (err) => {
-        console.error("File streaming error:", err);
-        res.status(404).send("File not found");
-      });
+//       stream.on("error", (err) => {
+//         console.error("File streaming error:", err);
+//         res.status(404).send("File not found");
+//       });
 
-      stream.pipe(res);
-    } else {
-      console.error("Error fetching chapeter:", Chapeter);
-      res.status(404).send("No chapeter of that name found");
-    }
-  } catch (error) {
-    console.error("Error fetching chapeter:", error);
-    res.status(500).send("Server error");
-  }
-});
+//       stream.pipe(res);
+//     } else {
+//       console.error("Error fetching chapeter:", Chapeter);
+//       res.status(404).send("No chapeter of that name found");
+//     }
+//   } catch (error) {
+//     console.error("Error fetching chapeter:", error);
+//     res.status(500).send("Server error");
+//   }
+// });
