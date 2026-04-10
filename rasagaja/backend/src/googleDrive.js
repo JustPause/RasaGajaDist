88019e398008a24c628468f904d41454ef3cd453
Justinas <iamjuststan@hotmail.com>
2026-04-10 21:34:36 +0300

    Pudate

--- rasagaja/backend/src/googleDrive.js
+++ rasagaja/backend/src/googleDrive.js
@@ -1,4 +1,5 @@
-const fs = require("node:fs").promises;
+// const fs = require("node:fs").promises;
+const fs = require("node:fs");
 const path = require("path");
 const process = require("process");
 
